ba8108308f970a3f9410b77e69d9e764c4dbc533
Vinnie Falco <vinnie.falco@gmail.com>
2012-04-22 15:08:44 -0700

    Fix midiplayer to use sendBlockOfMessages()

--- MIDIPlayer/Source/MidiPlayer.cpp
+++ MIDIPlayer/Source/MidiPlayer.cpp
@@ -1,12 +1,12 @@
 /*
 ================================================================================
-            MIDI Playback
+MIDI Playback
 
-    Author: SwingCoder
-    Email: SwingCoder2@gmail.com
-    Github: https://github.com/SwingCoder
+Author: SwingCoder
+Email: SwingCoder2@gmail.com
+Github: https://github.com/SwingCoder
 
-    License: MIT License, Copyright (c) 2012 by SwingCoder
+License: MIT License, Copyright (c) 2012 by SwingCoder
 ================================================================================
 */
 
@@ -25,57 +25,66 @@ MidiPlayer::~MidiPlayer()
 //=================================================================================
 void MidiPlayer::selectMidiDevice()
 {
-    // get all midi output deveices and add them to menu.
-    StringArray midiDevices = MidiOutput::getDevices();
-    PopupMenu menu;
+  // get all midi output deveices and add them to menu.
+  StringArray midiDevices = MidiOutput::getDevices();
+  PopupMenu menu;
 
-    for (int i = 0; i < midiDevices.size(); ++i)
-    {
-        menu.addItem(i + 1, midiDevices[i], true, midiOutput->getDefaultDeviceIndex() == i);        
-    }
+  for (int i = 0; i < midiDevices.size(); ++i)
+  {
+    menu.addItem(i + 1, midiDevices[i], true, midiOutput->getDefaultDeviceIndex() == i);        
+  }
 
-    // set midi output device to used.
-    midiOutput = MidiOutput::openDevice(menu.show());
+  // set midi output device to used.
+  midiOutput = MidiOutput::openDevice(menu.show());
 }
 //=================================================================================
+
 void MidiPlayer::playMidi()
 {
-    if (midiOutput != nullptr)
+  if (midiOutput != nullptr)
+  {
+    // Start the background thread right away
+    midiOutput->startBackgroundThread();
+
+    // Open up our file
+    FileInputStream fileStream (File ("E://00.mid")); 
+
+    // Now read the MIDI data
+    MidiFile midiFile;
+    midiFile.readFrom (fileStream);  
+
+    // We will convert the entire file into one MidiBuffer
+    MidiBuffer b;
+
+    // Loop over all tracks
+    for (int trackIndex = 0; trackIndex < midiFile.getNumTracks (); ++trackIndex)
     {
-        MidiFile midiFile;
-        const MidiMessageSequence* midiMessageSeque;
-        MidiMessage message;
-        MidiBuffer buffer;
+      MidiMessageSequence const* track = midiFile.getTrack (trackIndex);
 
-        // get midi file..        
-        FileInputStream fileStream(File("E://00.mid")); 
-        midiFile.readFrom(fileStream);  
-        midiFile.convertTimestampTicksToSeconds();
+      // Loop over all events
+      for (int index = 0; index < track->getNumEvents (); ++index)
+      {
+        // Add the message, preserving the time stamp
 
-        midiOutput->startBackgroundThread();
+        MidiMessage m = track->getEventPointer (index)->message;
+        b.addEvent (m, m.getTimeStamp ());
 
-        for (int i = 0; i < midiFile.getNumTracks(); ++i)
-        {
-            midiMessageSeque = midiFile.getTrack(i);
-
-            for (int i = 0; i < midiMessageSeque->getNumEvents(); ++i)
-            {
-                message = midiMessageSeque->getEventPointer(i)->message;
-                buffer.addEvent(message, midiMessageSeque->getNumEvents());
-            }  
-        }
-        
-        midiOutput->sendBlockOfMessages(buffer, 2000.0, 22050.0);
-
-        buffer.clear();
+        // For debugging
+        //Logger::outputDebugString (String (m.getTimeStamp ()));
+      }
     }
+
+    // Now send the entire block of messages, have it start 0.1 seconds from now.
+    midiOutput->sendBlockOfMessages(b, Time::getMillisecondCounter() + 100, 1000);
+  }
 }
+
 //=================================================================================
 void MidiPlayer::stopMidi()
 {
-    if (midiOutput != nullptr)
-    {
-        midiOutput->clearAllPendingMessages();
-        midiOutput->stopBackgroundThread();
-    }
+  if (midiOutput != nullptr)
+  {
+    midiOutput->clearAllPendingMessages();
+    midiOutput->stopBackgroundThread();
+  }
 }
