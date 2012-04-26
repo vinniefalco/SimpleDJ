aeca423eb1a61fdaf24f89538d56c3e4eb826493
SwingCoder <SwingCoder2@gmail.com>
2012-04-27 01:57:21 +0800

    Add: GuiCallbackQueue class.

---
+++ SwingBeats/Source/GuiCallbackQueue.cpp
@@ -0,0 +1,49 @@
+﻿/*
+====================================================================================
+            Swing Beats
+
+    Author: SwingCoder
+    Email: SwingCoder2@gmail.com
+    Github: https://github.com/SwingCoder
+
+    License: MIT License, Copyright (c) 2012 by SwingCoder
+====================================================================================
+*/
+
+#include "../JuceLibraryCode/JuceHeader.h"
+#include "CallbackQueue.h"
+#include "GuiCallbackQueue.h"
+
+//==================================================================================
+
+GuiCallbackQueue::GuiCallbackQueue()
+{
+}
+
+//==================================================================================
+
+GuiCallbackQueue::~GuiCallbackQueue()
+{
+}
+
+//==================================================================================
+
+void GuiCallbackQueue::handleAsyncUpdate()
+{
+    process();
+}
+
+//==================================================================================
+
+void GuiCallbackQueue::signal()
+{
+    triggerAsyncUpdate();
+}
+
+//==================================================================================
+
+void GuiCallbackQueue::reset()
+{
+}
+
+
---
+++ SwingBeats/Source/GuiCallbackQueue.h
@@ -0,0 +1,32 @@
+﻿/*
+====================================================================================
+            Swing Beats
+
+    Author: SwingCoder
+    Email: SwingCoder2@gmail.com
+    Github: https://github.com/SwingCoder
+
+    License: MIT License, Copyright (c) 2012 by SwingCoder
+====================================================================================
+*/
+
+#ifndef GUICALLBACKQUEUE_H_
+#define GUICALLBACKQUEUE_H_
+
+class GuiCallbackQueue : public AsyncUpdater, public CallbackQueue
+{
+public:
+    GuiCallbackQueue();
+    ~GuiCallbackQueue();
+
+    virtual void handleAsyncUpdate();
+
+    virtual void signal();
+    virtual void reset();
+
+
+};
+
+
+
+#endif // GUICALLBACKQUEUE_H_
